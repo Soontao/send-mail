81ab1d5ed3408c28bc7d2b444de88aa6d521cb1c
Theo Sun <theo.sun@sap.com>
2017-06-28 12:48:44 +0800

    update

--- README.md
+++ README.md
@@ -19,3 +19,23 @@ live sample
 ```bash
 send-email-to -s QQ -u no.such.mail.box@qq.com -p nosuchmailpass -t theo.sun@outlook.com -h 'HI Theo' -c 'this is content, nice to see you theo'
 ```
+
+## Adavance
+
+With config
+
+```bash
+send-mail --save -s QQ -u monitor@fornever.org -p gbyolpjzwqtfbibe -t theo.sun@qq.com
+```
+
+And you can use the saved config later
+
+```bash
+send-mail -h 'Hi theo' -c 'nice to see you here'
+```
+
+Delete config
+
+```bash
+send-mail --clean
+```
--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 #!/usr/bin/env node
-var Promise = require('bluebird')
-var nodeMailer = require('nodemailer')
-var argv = require('optimist').argv
+var nodeMailer = require("nodemailer");
+var argv = require("optimist").argv;
+var os = require("os");
+var fs = require("fs");
+var path = require("path");
+var fUserConfig = path.join(os.homedir(), "./.send_mail_config.json");
+
+if (argv.save) {
+  fs.writeFileSync(fUserConfig, JSON.stringify(argv, null, " "));
+  console.log("Config saved !");
+  process.exit();
+}
+
+if (argv.clean) {
+  fs.unlinkSync(fUserConfig);
+  console.log("Config deleted !");
+  process.exit();
+}
+
+if (fs.existsSync(fUserConfig)) {
+  argv = Object.assign(JSON.parse(fs.readFileSync(fUserConfig)), argv);
+}
 
 var transporter = nodeMailer.createTransport({
-  'service': argv.s,
-  'pool': 'false',
-  'auth': {
-    'user': argv.u,
-    'pass': argv.p
+  service: argv.s,
+  pool: "false",
+  auth: {
+    user: argv.u,
+    pass: argv.p
   }
 });
 
-transporter.sendMail({
-  from: `"${argv.u}" ${argv.u}`,
-  to: argv.t,
-  subject: argv.h,
-  text: argv.c
-}, (err, info) => {
-  if (err) {
-    console.error(err.message)
-    process.exit(1)
+transporter.sendMail(
+  {
+    from: `"${argv.u}" ${argv.u}`,
+    to: argv.t,
+    subject: argv.h,
+    text: argv.c
+  },
+  (err, info) => {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    } else {
+      console.log("Mail has been sent.");
+      process.exit();
+    }
   }
-  else {
-    console.log('Mail has been sent.')
-    process.exit()
-  }
-})
+);
